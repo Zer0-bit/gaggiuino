e5ba0cf788d839e431321a5e837c2de296f72bf3
mr.toor <42692077+Zer0-bit@users.noreply.github.com>
2021-04-25 22:15:35 +0100

    Update gaggiuino.ino
    Rethinking some of the controls

--- gaggiuino.ino
+++ gaggiuino.ino
@@ -9,41 +9,38 @@ int thermoCLK = 6;
 int relayPin = 8;
 
 // EEPROM  stuff
-const uint32_t COUNT_ADDR1 = 0;
-const uint32_t COUNT_ADDR2 = 20;
-float E_ADDR1 = 0;
-float E_ADDR2 = 0;
+int EEP_ADDR1 = 1;
+int EEP_ADDR2 = 20;
+int EEP_ADDR3 = 40;
+long EEP_VALUE1;
+long EEP_VALUE2;
+long EEP_VALUE3;
 uint32_t presetTemp;
 uint32_t offsetTemp;
-uint32_t brewTimeOffsetTemp;
-
-//Define the buffer used for the buttons sent to Nextion
-//char buffer[10] = {0};
+uint32_t brewTimeDelayTemp;
 
 //Define the temp reading vars
-float realTemp;
-float displayTemp;
 uint32_t lastSetBoilerTemp;
 uint32_t lastSetOffsetTemp;
-uint32_t savedOffsetTemp;
-uint32_t savedBoilerTemp;
 uint32_t waterTemp;
-uint32_t savedBrewTimeTemp;
 
 
 //Init the thermocouple with the appropriate pins
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 // Define the Nextion objects types
+NexText waterTextBox0 = NexText(0, 2, "t0");
+NexText boilerTextBox1 = NexText(0, 4, "t1");
 NexNumber boilerTemp = NexNumber(1, 4, "n0");
-NexNumber offTemp = NexNumber(1, 6, "n1");
-NexNumber brewTimeTemp = NexNumber(1, 13, "n2");
+NexNumber offTimeTemp = NexNumber(1, 6, "n1");
+NexNumber brewTimeDelay = NexNumber(1, 11, "n2");
 NexButton bPlus = NexButton(1, 3, "b2");
 NexButton bMinus = NexButton(1, 2, "b1");
 NexButton boffsetPlus = NexButton(1, 7, "b4");
 NexButton boffsetMinus = NexButton(1, 5, "b3");
 NexButton bSave = NexButton(1, 10, "b5");
 NexPage startPage = NexPage(1, 0, "page1");
+NexPage mainPage = NexPage(0, 0, "page0");
 
 //Register object n0, b0, b1, to the touch event list.
 NexTouch *nex_listen_list[] =
@@ -56,6 +53,19 @@ NexTouch *nex_listen_list[] =
   NULL  
 };
 
+// EEPROM WRITE
+void writeIntIntoEEPROM(int address, int number)
+{ 
+  EEPROM.update(address, number >> 8);
+  EEPROM.update(address + 1, number & 0xFF);
+}
+//EEPROM READ
+int readIntFromEEPROM(int address)
+{
+  return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
+}
+
+//Nextion stuff
 void bPlusPushCallback(void *ptr)
 {
   uint32_t presetTemp;
@@ -73,86 +83,103 @@ void bMinusPushCallback(void *ptr)
 void boffsetPlusPushCallback(void *ptr)
 {
   uint32_t offsetTemp;
-  offTemp.getValue(&offsetTemp); 
+  offTimeTemp.getValue(&offsetTemp); 
   offsetTemp++;
-  offTemp.setValue(offsetTemp);
+  offTimeTemp.setValue(offsetTemp);
 }
 void boffsetMinusPushCallback(void *ptr)
 {
   uint32_t offsetTemp;
-  offTemp.getValue(&offsetTemp);
+  offTimeTemp.getValue(&offsetTemp);
   offsetTemp--;
-  offTemp.setValue(offsetTemp);
+  offTimeTemp.setValue(offsetTemp);
 }
 void bSavePushCallback(void *ptr)
 {
-  //Get current set desired temp values and save them to EEPROM
-  boilerTemp.getValue(&savedBoilerTemp);
-  offTimeTemp.getValue(&savedOffsetTemp);
-  brewTimeTemp.getValue(&savedBrewTimeTemp);
-  E_ADDR1 = savedBoilerTemp;
-  E_ADDR2 = savedOffsetTemp;
-  E_ADDR3 = savedBrewTimeTemp;
-  EEPROM.update(COUNT_ADDR1, E_ADDR1);
-  EEPROM.update(COUNT_ADDR2, E_ADDR2);
-  EEPROM.update(COUNT_ADDR3, E_ADDR3);
+  // set desired temp values and save them to EEPROM
+  int savedOffsetTemp;
+  int savedBoilerTemp;
+  int savedBrewTimeDelay;
+  boilerTemp.getValue(&EEP_VALUE1);
+  offTimeTemp.getValue(&EEP_VALUE2);
+  brewTimeDelay.getValue(&EEP_VALUE3);
+  savedBoilerTemp = EEP_VALUE1 / 4;
+  savedOffsetTemp = EEP_VALUE2 / 4;
+  savedBrewTimeDelay = EEP_VALUE3 / 4;
+  if ((savedBoilerTemp != 0) && (savedOffsetTemp != 0) && (savedBrewTimeDelay != 0)) {
+    writeIntIntoEEPROM(EEP_ADDR1, savedBoilerTemp);
+    writeIntIntoEEPROM(EEP_ADDR2, savedOffsetTemp);
+    writeIntIntoEEPROM(EEP_ADDR3, savedBrewTimeDelay);
+  }
+  else {
+  }
   
 }
-void setup() {
-  Serial.begin(9600);
-  nexInit();
-  pinMode(relayPin, OUTPUT);
-  // wait for EEPROM and other chip to stabilize
-  startPage.show();
-  delay(2000);
-  bPlus.attachPush(bPlusPushCallback, &bPlus);
-  bMinus.attachPush(bMinusPushCallback, &bMinus);
-  boffsetPlus.attachPush(boffsetPlusPushCallback, &boffsetPlus);
-  boffsetMinus.attachPush(boffsetMinusPushCallback, &boffsetMinus);
-  bSave.attachPush(bSavePushCallback, &bSave);
-  boilerTemp.setValue(EEPROM.read(COUNT_ADDR1));
-  offTemp.setValue(EEPROM.read(COUNT_ADDR2));  
-  brewTimeTemp.setValue(EEPROM.read(COUNT_ADDR3));
-}
-
-void loop() {
-  nexLoop(nex_listen_list);
-  
-// Readig the preset and offset temperature values set on the Nextion Menu page
+void doCoffee() {
   boilerTemp.getValue(&presetTemp);
-  offTemp.getValue(&offsetTemp);
-  brewTimeTemp.getValue(&brewTimeOffsetTemp);
-  
-// some logic to keep the boiler as close to the desired set temp as possible 
+  offTimeTemp.getValue(&offsetTemp);
+  brewTimeDelay.getValue(&brewTimeDelayTemp);
+
+ // some logic to keep the boiler as close to the desired set temp as possible 
   waterTemp=presetTemp-float(offsetTemp);
   if (thermocouple.readCelsius() < float(presetTemp-10)) {
     digitalWrite(relayPin, HIGH);
   }
   else if (thermocouple.readCelsius() >= float(presetTemp-10) && thermocouple.readCelsius() < float(presetTemp)) {
-    // Smth that resembles PWM control aka software pseudo-pwm
-    // Change the <brewTimeOffsetTemp> to keep your machine temps closer to the desired range i ended up with 1200ms on a 2014 Gaggia
     digitalWrite(relayPin, HIGH);
-    delay(brewTimeOffsetTemp);
+    delay(brewTimeDelayTemp);
     digitalWrite(relayPin, LOW);
   }
   else {
     digitalWrite(relayPin, LOW);
   }
-  
-  // Writing the temp values on the Nextion textboxes
-  realTemp = thermocouple.readCelsius();
-  String realTempPrint = "t1.txt=\""+String(realTemp,1)+"\"";
-  Serial.print(realTempPrint);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  displayTemp = realTemp - float(offsetTemp);
-  String waterTempPrint = "t0.txt=\""+String(displayTemp,1)+"\"";
-  Serial.print(waterTempPrint);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  
-  // Needed so the thermocouple has time to do the temps calculations
+}
+void update_t0_water() {
+  float tmp = thermocouple.readCelsius();
+  String displayTemp;
+  displayTemp = tmp - float(offsetTemp);
+  //waterTempPrint = "t0.txt=\""+String(displayTemp,1)+"\"";
+  char const* waterTempPrint = displayTemp.c_str();
+  waterTextBox0.setText(waterTempPrint);
+}
+void update_t1_boiler() {
+  String realTemp;
+  float tmp = thermocouple.readCelsius();
+  realTemp = tmp;
+  char const* realTempPrint = realTemp.c_str();
+  boilerTextBox1.setText(realTempPrint);
+}
+void setup() {
+  Serial.begin(9600); 
+  nexInit();
+  startPage.show();
+  bPlus.attachPush(bPlusPushCallback, &bPlus);
+  bMinus.attachPush(bMinusPushCallback, &bMinus);
+  boffsetPlus.attachPush(boffsetPlusPushCallback, &boffsetPlus);
+  boffsetMinus.attachPush(boffsetMinusPushCallback, &boffsetMinus);
+  bSave.attachPush(bSavePushCallback, &bSave);
+  // port setup
+  pinMode(relayPin, OUTPUT);  
+  // port init
+  digitalWrite(relayPin, LOW);
+
+  // wait for EEPROM and other chip to stabilize
+  delay(2000);  
+  uint32_t tmp1 = readIntFromEEPROM(EEP_ADDR1);
+  uint32_t tmp2 = readIntFromEEPROM(EEP_ADDR2);
+  uint32_t tmp3 = readIntFromEEPROM(EEP_ADDR3);
+  if ((tmp1 != 0) && (tmp2 != 0) && (tmp3 != 0)) {
+    boilerTemp.setValue(tmp1);
+    offTimeTemp.setValue(tmp2);
+    brewTimeDelay.setValue(tmp3);
+    mainPage.show();
+  }
+}
+
+void loop() {
+  nexLoop(nex_listen_list);
+  doCoffee();
+  update_t0_water();
+  update_t1_boiler();
   delay(250);
 }
