11749b283532eadf6d31a26bddc31693220089e1
mr.toor <42692077+Zer0-bit@users.noreply.github.com>
2021-05-29 00:20:18 +0100

    adding pre-infusion and brew detect
    Required additional hw for this to work

--- gaggiuino.ino
+++ gaggiuino.ino
@@ -2,26 +2,91 @@
 #include <trigger.h>
 #include <EasyNextionLibrary.h>
 #include <max6675.h>
+#include <RBDdimmer.h>
 
 // Define our pins
 int thermoDO = 4;
 int thermoCS = 5;
 int thermoCLK = 6;
+bool vibroPin = 7;
 int relayPin = 8;
+bool preinfusionEnabled;
+
+// Dimmer stuff
+#define outputPin  9 
+#define zerocross  5
 
 //Init the thermocouple with the appropriate pins
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 EasyNex myNex(Serial);
+dimmerLamp dimmer(outputPin);
 
 // EEPROM  stuff
 int EEP_ADDR1 = 1, EEP_ADDR2 = 20, EEP_ADDR3 = 40, EEP_ADDR4 = 60;
 unsigned long EEP_VALUE1, EEP_VALUE2, EEP_VALUE3, EEP_VALUE4;
 
-int i = 0;
+int i = 0, preinfusionRunCheck = 0;
 uint32_t setPoint, offsetTemp, brewTimeDelayTemp, brewTimeDelayDivider, realTempRead, waterTemp;
 float const MAX_TEMP = 120;
 const int MAX = 100;
 
+// Main loop
+void setup() {
+  // Serial communication init
+  myNex.begin(9600);
+
+  // Port init - starts with the relay decoupled
+  digitalWrite(relayPin, LOW);
+
+  // Dimmer lib init
+  dimmer.begin(NORMAL_MODE, ON);
+  // nexInit();
+  delay(100);
+  //Registering a higher baud for hopefully more responsive touch controls
+  Serial.print("baud=115200");
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.write(0xff);
+  Serial.end();
+  Serial.begin(115200);
+
+  // port setup
+  pinMode(relayPin, OUTPUT);
+  pinMode(vibroPin, INPUT); 
+
+  // Small delay so the LCD has a chance to fully initialize before passing the EEPROM values
+  delay(600);
+
+  preinfusionEnabled = myNex.readNumber("c0");
+  // Applying the EEPROM saved values  
+  uint32_t tmp1 = readIntFromEEPROM(EEP_ADDR1);
+  uint32_t tmp2 = readIntFromEEPROM(EEP_ADDR2);
+  uint32_t tmp3 = readIntFromEEPROM(EEP_ADDR3);
+  uint32_t tmp4 = readIntFromEEPROM(EEP_ADDR4);
+  if ((tmp1 != 0) && (tmp2 != 0) && (tmp3 != 0) && (tmp4 != 0)) {
+    myNex.writeNum("page1.n0.val",tmp1);
+    myNex.writeNum("page1.n1.val",tmp2);
+    myNex.writeNum("page2.n0.val",tmp3);
+    myNex.writeNum("page2.n1.val",tmp4);
+  }
+  delay(50);
+}
+
+void loop() {
+  //Main loop where all the above logic is continuously run
+  myNex.NextionListen();
+  do {
+    doCoffee();    
+  }
+  while (preinfusionEnabled == false);
+  do {
+    doCoffeeWithPreinfusion();
+  }
+  while (preinfusionEnabled == true);
+  updateLCD();
+  preinfusionStop();
+  delay(250);
+}
 
 // EEPROM WRITE
 void writeIntIntoEEPROM(int address, int number)
@@ -29,44 +94,13 @@ void writeIntIntoEEPROM(int address, int number)
   EEPROM.update(address, number >> 8);
   EEPROM.update(address + 1, number & 0xFF);
 }
+
 //EEPROM READ
 int readIntFromEEPROM(int address)
 {
   return (EEPROM.read(address) << 8) + EEPROM.read(address + 1);
 }
 
-// Button minus
-void trigger1()
-{
-  uint32_t setPoint;
-  setPoint = myNex.readNumber("page1.n0.val");
-  setPoint--;
-  myNex.writeNum("page1.n0.val",setPoint);
-}
-// Button plus
-void trigger2()
-{
-  uint32_t setPoint;
-  setPoint = myNex.readNumber("page1.n0.val");
-  setPoint++;
-  myNex.writeNum("page1.n0.val",setPoint);
-}
-// Button minus
-void trigger3()
-{
-  uint32_t offsetTemp;
-  offsetTemp = myNex.readNumber("page1.n1.val");
-  offsetTemp--;
-  myNex.writeNum("page1.n1.val",offsetTemp);
-}
-// Button plus
-void trigger4()
-{
-  uint32_t offsetTemp;
-  offsetTemp = myNex.readNumber("page1.n1.val");
-  offsetTemp++;
-  myNex.writeNum("page1.n1.val",offsetTemp);
-}
 // Save the desired temp values to EEPROM
 void trigger5()
 {
@@ -81,6 +115,78 @@ void trigger5()
   writeIntIntoEEPROM(EEP_ADDR4, savedbrewTimeDivider);
   myNex.writeStr("popupMSG.t0.txt", "SUCCESS!");
   myNex.writeStr("page popupMSG");
+}
+
+// Preinfusion logic
+void preinfusion() {
+  int preinfuseTimer = myNex.readNumber("page2.n1.val");
+  while (preinfusionRunCheck < 1) {
+    uint32_t period = preinfuseTimer * 1000;  // calc seconds
+    // Dim the pump for *period ammount of seconds
+    for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
+      dimmer.setPower(20);
+    }
+    
+    uint32_t pause = 3 * 1000;  // calc seconds
+    // Dim the pump for *pause ammount of seconds
+    for( uint32_t tStart = millis();  (millis()-tStart) < pause;  ){
+      dimmer.setPower(0);
+    }
+    dimmer.setPower(100);
+    preinfusionRunCheck++;
+  }
+}
+
+// Detect when brewing stops
+void preinfusionStop() {
+  if (vibroPin != true) preinfusionRunCheck = 0;
+}
+
+// EXPERIMENTAL NOT IN USE NOW 
+void doCoffeeWithPreinfusion() {
+  // Getting the operational readings
+  float currentTempReadValue = thermocouple.readCelsius();
+  setPoint = myNex.readNumber("page1.n0.val");
+  offsetTemp = myNex.readNumber("page1.n1.val");
+  brewTimeDelayTemp = myNex.readNumber("page2.n0.val");
+  brewTimeDelayDivider = myNex.readNumber("page2.n1.val");
+
+  // Calculating the boiler heating power
+  int powerOutput = map(currentTempReadValue, setPoint-10, setPoint, brewTimeDelayTemp, brewTimeDelayTemp/brewTimeDelayDivider);
+  while (vibroPin == true ) {
+    preinfusion();
+    if (powerOutput > brewTimeDelayTemp){
+      powerOutput = brewTimeDelayTemp;
+    }
+    else if (powerOutput < brewTimeDelayTemp/brewTimeDelayDivider) {
+      powerOutput = brewTimeDelayTemp/brewTimeDelayDivider;
+    }
+
+    // Applying the powerOutput variable as part of the relay logic
+    if (currentTempReadValue < float(setPoint-10)) {
+      digitalWrite(relayPin, HIGH);
+    }
+    else if (currentTempReadValue >= float(setPoint-10) && currentTempReadValue < float(setPoint-3)) {
+      digitalWrite(relayPin, HIGH);
+      delay(powerOutput);
+      digitalWrite(relayPin, LOW);
+    }
+    else if (currentTempReadValue >= float(setPoint-3) && currentTempReadValue <= float(setPoint-1)) {
+      digitalWrite(relayPin, HIGH);
+      delay(powerOutput);
+      digitalWrite(relayPin, LOW);
+      delay(powerOutput);
+    }
+    else if (currentTempReadValue >= float(setPoint-1) && currentTempReadValue < float(setPoint-0.2)) {
+      digitalWrite(relayPin, HIGH);
+      delay(powerOutput);
+      digitalWrite(relayPin, LOW);
+      delay(powerOutput);
+    }
+    else {
+      digitalWrite(relayPin, LOW);
+    }
+  }
 }
 
 // The *precise* temp control logic
@@ -102,21 +208,21 @@ void doCoffee() {
   }
 
   // Applying the powerOutput variable as part of the relay logic
-  if (thermocouple.readCelsius() < float(setPoint-10)) {
+  if (currentTempReadValue < float(setPoint-10)) {
     digitalWrite(relayPin, HIGH);
   }
-  else if (thermocouple.readCelsius() >= float(setPoint-10) && thermocouple.readCelsius() < float(setPoint-3)) {
-    digitalWrite(relayPin, HIGH);
-    delay(powerOutput);
-    digitalWrite(relayPin, LOW);
-  }
-  else if (thermocouple.readCelsius() >= float(setPoint-3) && thermocouple.readCelsius() <= float(setPoint-1)) {
+  else if (currentTempReadValue >= float(setPoint-10) && currentTempReadValue < float(setPoint-3)) {
     digitalWrite(relayPin, HIGH);
     delay(powerOutput);
     digitalWrite(relayPin, LOW);
+  }
+  else if (currentTempReadValue >= float(setPoint-3) && currentTempReadValue <= float(setPoint-1)) {
+    digitalWrite(relayPin, HIGH);
+    delay(powerOutput);
+    digitalWrite(relayPin, LOW);
     delay(powerOutput);
   }
-  else if (thermocouple.readCelsius() >= float(setPoint-1) && thermocouple.readCelsius() < float(setPoint-0.2)) {
+  else if (currentTempReadValue >= float(setPoint-1) && currentTempReadValue < float(setPoint-0.2)) {
     digitalWrite(relayPin, HIGH);
     delay(powerOutput);
     digitalWrite(relayPin, LOW);
@@ -161,45 +267,4 @@ void updateLCD() {
   //Printing the current values to the display
   waterTempPrint = currentTempReadValue - float(offsetTemp);
   myNex.writeStr("page0.t0.txt", waterTempPrint);
-}
-void setup() {
-  
-  myNex.begin(9600);  
-  // nexInit();
-  delay(100);
-  //Registering a higher baud for hopefully more responsive touch controls
-  Serial.print("baud=115200");
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.write(0xff);
-  Serial.end();
-  Serial.begin(115200);
-
-  // port setup
-  pinMode(relayPin, OUTPUT);  
-  // port init - starts with the relay decoupled just in case
-  digitalWrite(relayPin, LOW);
-
-  // Small delay so the LCD has a chance to fully initialize before passing the EEPROM values
-  delay(900);
-
-  // Applying the EEPROM saved values  
-  uint32_t tmp1 = readIntFromEEPROM(EEP_ADDR1);
-  uint32_t tmp2 = readIntFromEEPROM(EEP_ADDR2);
-  uint32_t tmp3 = readIntFromEEPROM(EEP_ADDR3);
-  uint32_t tmp4 = readIntFromEEPROM(EEP_ADDR4);
-  if ((tmp1 != 0) && (tmp2 != 0) && (tmp3 != 0) && (tmp4 != 0)) {
-    myNex.writeNum("page1.n0.val",tmp1);
-    myNex.writeNum("page1.n1.val",tmp2);
-    myNex.writeNum("page2.n0.val",tmp3);
-    myNex.writeNum("page2.n1.val",tmp4);
-  }
-}
-
-void loop() {
-  //Main loop where all the above logic is continuously run
-  myNex.NextionListen();
-  doCoffee();
-  updateLCD();
-  delay(300);
 }
