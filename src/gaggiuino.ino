633e486906c02b8a7b90428e00b891b5dee79f4b
Zer0-bit <toor@root.com>
2022-04-11 11:36:58 +0100

    + external adc config

--- src/gaggiuino.ino
+++ src/gaggiuino.ino
@@ -320,17 +320,17 @@ void initPressure(unsigned int hz) {
 }
 
 float getPressure() {  //returns sensor pressure data
-    // 5V/1024 = 1/204.8
-    // voltageZero = 0.5V --> 102.4
-    // voltageMax = 4.5V --> 921.6
-    // range 921.6 - 102.4 = 819.2
+    // 5V/1024 = 1/204.8 (10 bit) or 6553.6 (15 bit) 
+    // voltageZero = 0.5V --> 102.4(10 bit) or 3276.8 (15 bit)
+    // voltageMax = 4.5V --> 921.6 (10 bit) or 29491.2 (15 bit) 
+    // range 921.6 - 102.4 = 819.2 or 26214.4
     // pressure gauge range 0-1.2MPa - 0-12 bar
-    // 1 bar = 68.27
+    // 1 bar = 68.27 or 2184.5
 
     #if defined(ARDUINO_ARCH_AVR)
       return (presData[0] + presData[1]) / 136.54f - 1.49f;
-    #else
-      return ADS.getValue() / 68.27f - 1.49f;
+    #elif defined(ARDUINO_ARCH_STM32)
+      return ADS.getValue() / 2184.5f - 1.49f;
     #endif
 }
 
