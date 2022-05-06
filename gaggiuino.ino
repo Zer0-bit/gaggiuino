f0dd257c0197fc7abfe8e2ad64e9ea326fa4c127
Martin Hrůza <martin.hruza123@gmail.com>
2022-05-06 23:50:33 +0200

    Remove unused function smoothValue()

--- gaggiuino.ino
+++ gaggiuino.ino
@@ -799,44 +799,6 @@ float mapRange(float sourceNumber, float fromA, float fromB, float toA, float to
   return (float) round(finalNumber * calcScale) / calcScale;
 }
 
-
-float smoothValue(float inputVal) {
-  // Define the number of samples to keep track of. The higher the number, the
-  // more the readings will be smoothed, but the slower the output will respond to
-  // the input. Using a constant rather than a normal variable lets us use this
-  // value to determine the size of the readings array.
-  const int numReadings = 5;
-
-  int readings[numReadings];      // the readings from the analog input
-  int readIndex = 0;              // the index of the current reading
-  int total = 0;                  // the running total
-  int average = 0;                // the average
-
-  // initialize serial communication with computer:
-  // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
-  // subtract the last reading:
-  total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = inputVal;
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
-  }
-
-  // calculate the average:
-  return average = total / numReadings;
-}
-
-
 //#############################################################################################
 //###############################____DESCALE__CONTROL____######################################
 //#############################################################################################
