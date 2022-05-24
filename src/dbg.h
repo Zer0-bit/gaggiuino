2e8ffc3495651fcd890ff4b214b9e78f658a408f
Zer0-bit <toor@root.com>
2022-05-24 17:17:34 +0100

    - accidental cpp file deletion

--- src/dbg.h
+++ src/dbg.h
@@ -12,20 +12,20 @@
 
 static int readVref() {
 #ifdef __LL_ADC_CALC_VREFANALOG_VOLTAGE
-    return (__LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION));
+  return (__LL_ADC_CALC_VREFANALOG_VOLTAGE(analogRead(AVREF), LL_ADC_RESOLUTION));
 #else
-    return (VREFINT * ADC_RANGE / analogRead(AVREF)); // ADC sample to mV
+  return (VREFINT * ADC_RANGE / analogRead(AVREF)); // ADC sample to mV
 #endif
 }
 
 #ifdef ATEMP
 static int readTempSensor(int32_t VRef) {
 #ifdef __LL_ADC_CALC_TEMPERATURE
-    return (__LL_ADC_CALC_TEMPERATURE(VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
+  return (__LL_ADC_CALC_TEMPERATURE(VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
 #elif defined(__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS)
-    return (__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS(AVG_SLOPE, V25, CALX_TEMP, VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
+  return (__LL_ADC_CALC_TEMPERATURE_TYP_PARAMS(AVG_SLOPE, V25, CALX_TEMP, VRef, analogRead(ATEMP), LL_ADC_RESOLUTION));
 #else
-    return 0;
+  return 0;
 #endif
 }
 #endif
