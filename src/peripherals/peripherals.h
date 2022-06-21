f7ddc91fa9cb2e655128f1e413c54efff206c85d
Martin Hrůza <martin.hruza123@gmail.com>
2022-06-21 22:36:15 +0200

    Optimization of peripheral.c

--- src/peripherals/peripherals.h
+++ src/peripherals/peripherals.h
@@ -1,15 +1,45 @@
 #ifndef PERIPHERALS_H
 #define PERIPHERALS_H
 
-void pinInit(void);
+#include "pindef.h"
+#include "peripherals.h"
+#include <Arduino.h>
 
-bool brewState(void);
-bool steamState(void);
+void pinInit(void) {
+  pinMode(relayPin, OUTPUT);
+  pinMode(brewPin,  INPUT_PULLUP);
+  pinMode(steamPin, INPUT_PULLUP);
+  pinMode(HX711_dout_1, INPUT_PULLUP);
+  pinMode(HX711_dout_2, INPUT_PULLUP);
+}
 
-void setBoilerOn(void);
-void setBoilerOff(void);
+// Actuating the heater element
+static inline void setBoilerOn(void) {
+  digitalWrite(relayPin, HIGH);  // boilerPin -> HIGH
+}
 
-void closeValve(void);
-void openValve(void);
+static inline void setBoilerOff(void) {
+  digitalWrite(relayPin, LOW);  // boilerPin -> LOW
+}
+
+//Function to get the state of the brew switch button
+//returns true or false based on the read P(power) value
+static inline bool brewState(void) {  //Monitors the current flowing through the ACS712 circuit and returns a value depending on the power value (P) the system draws
+  return digitalRead(brewPin) == LOW; // pin will be low when switch is ON.
+}
+
+// Returns HIGH when switch is OFF and LOW when ON
+// pin will be high when switch is ON.
+static inline bool steamState(void) {
+  return digitalRead(steamPin) == LOW; // pin will be low when switch is ON.
+}
+
+static inline void openValve(void) {
+  digitalWrite(valvePin, HIGH);
+}
+
+static inline void closeValve(void) {
+  digitalWrite(valvePin, LOW);
+}
 
 #endif
