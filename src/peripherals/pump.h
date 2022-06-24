221636f3aeca9ce14af170d6b63ca584244e24f2
Martin Hrůza <martin.hruza123@gmail.com>
2022-06-24 14:10:53 +0200

    Fix setPumpPressure's parameters order

--- src/peripherals/pump.h
+++ src/peripherals/pump.h
@@ -6,7 +6,7 @@
 #define PUMP_RANGE 127
 #define ZC_MODE    RISING
 
-void setPumpPressure(float targetValue, float livePressure);
+void setPumpPressure(float livePressure, float targetValue);
 void setPumpOff(void);
 void setPumpToRawValue(uint8_t val);
 
