987e16762a6435e6c1989a9af6b9a0fd2ef00f52
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2022-07-19 19:32:39 +0800

    Fix tests

--- tests/test_pressure_profiler.cpp
+++ tests/test_pressure_profiler.cpp
@@ -1,16 +1,22 @@
 #include <unity.h>
-#include "PressureProfile.h"
+#include "profiling_phases.h"
 #include "utils.h"
 
+
+Phase pressurePhase(float start, float end, unsigned int time) {
+  return Phase{PHASE_TYPE_PRESSURE, start, end, 0.f, 0.f, time};
+}
+
 Phase phaseArray[] = {
-    Phase{0, 2, 1000},
-    Phase{2, 2, 10000},
-    Phase{2, 9, 1000},
-    Phase{9, 9, 10000},
-    Phase{9, 6, 30000},
+    pressurePhase(0, 2, 1000),
+    pressurePhase(2, 2, 10000),
+    pressurePhase(2, 9, 1000),
+    pressurePhase(9, 9, 10000),
+    pressurePhase(9, 6, 30000),
 };
 
 Phases phases = {5, phaseArray};
+
 
 void test_current_phase_calculation(void)
 {
@@ -39,35 +45,40 @@ void test_current_phase_calculation(void)
 
 void test_get_pressure_for_phase(void)
 {
-    Phase phase = Phase{0, 2, 1000};
+    Phase phase = pressurePhase(0, 2, 1000);
 
-    TEST_ASSERT_EQUAL_FLOAT(0.0f, phase.getPressure(0));
-    TEST_ASSERT_EQUAL_FLOAT(1.0f, phase.getPressure(500));
-    TEST_ASSERT_EQUAL_FLOAT(1.5f, phase.getPressure(750));
-    TEST_ASSERT_EQUAL_FLOAT(2.0f, phase.getPressure(1000));
+    TEST_ASSERT_EQUAL_FLOAT(0.0f, phase.getTarget(0));
+    TEST_ASSERT_EQUAL_FLOAT(1.0f, phase.getTarget(500));
+    TEST_ASSERT_EQUAL_FLOAT(1.5f, phase.getTarget(750));
+    TEST_ASSERT_EQUAL_FLOAT(2.0f, phase.getTarget(1000));
 }
 
 
 void test_get_pressure_for_phase_with_negative_change(void)
 {
-    Phase phase = Phase{9, 6, 3000};
+    Phase phase = pressurePhase(9, 6, 3000);
 
-    TEST_ASSERT_EQUAL_FLOAT(9.0f, phase.getPressure(0));
-    TEST_ASSERT_EQUAL_FLOAT(8.0f, phase.getPressure(1000));
-    TEST_ASSERT_EQUAL_FLOAT(7.0f, phase.getPressure(2000));
-    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getPressure(3000));
+    TEST_ASSERT_EQUAL_FLOAT(9.0f, phase.getTarget(0));
+    TEST_ASSERT_EQUAL_FLOAT(8.0f, phase.getTarget(1000));
+    TEST_ASSERT_EQUAL_FLOAT(7.0f, phase.getTarget(2000));
+    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getTarget(3000));
 }
 
 void test_get_pressure_for_phase_with_time_larger_than_duration(void)
 {
-    Phase phase = Phase{9, 6, 3000};
+    Phase phase = pressurePhase(9, 6, 3000);
 
-    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getPressure(4000));
-    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getPressure(10000));
+    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getTarget(4000));
+    TEST_ASSERT_EQUAL_FLOAT(6.0f, phase.getTarget(10000));
 }
 
 void test_phases_with_zero_duration_are_skipped(void) {
-    Phase array[] = {Phase{2, 2, 0}, Phase{2, 5, 0}, Phase{2,5,0}, Phase {5, 5, 1000}};
+    Phase array[] = {
+      pressurePhase(2, 2, 0),
+      pressurePhase(2, 5, 0),
+      pressurePhase(2,5,0),
+      pressurePhase(5, 5, 1000)
+    };
     Phases phases = Phases {4, array};
 
     TEST_ASSERT_EQUAL(3, phases.getCurrentPhase(0).phaseIndex);
