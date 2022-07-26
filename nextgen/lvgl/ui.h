0aa1013da02e6ebe343408d48a5c74071beecc13
Zer0-bit <toor@root.com>
2022-07-26 21:55:12 +0100

    redesigning init screen

--- nextgen/lvgl/ui.h
+++ nextgen/lvgl/ui.h
@@ -1,7 +1,9 @@
 #include <lvgl.h>
 #include <Arduino.h>
 
-LV_IMG_DECLARE(ui_img_gaggiuin_no_o_logo_transp_png);    //GAGGIUIN_NO_O_LOGO_transp.png
+// Assets
+LV_IMG_DECLARE(ui_img_gaggiuino_icon_transp_png);    //GAGGIUIN_NO_O_LOGO_transp.png
+LV_FONT_DECLARE(fontHomeScreenTempArc);
 
 void ui_init(void);
 void lcd_init(void);
