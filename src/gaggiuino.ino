26591f02452e9485ba9d25ae73d45c89f15ad7d9
Zer0-bit <toor@root.com>
2022-09-13 18:59:13 +0100

    smoothed flow

--- src/gaggiuino.ino
+++ src/gaggiuino.ino
@@ -162,7 +162,7 @@ static void calculateWeightAndFlow(void) {
         previousWeight = shotWeight;
       } else {
         if (preinfusionFinished /*&& !currentState.isPressureRaising*/) {
-          shotWeight += currentState.pumpFlow * elapsedTime / 1000;
+          shotWeight += smoothedPumpFlow * elapsedTime / 1000;
         }
       }
 
@@ -179,13 +179,13 @@ bool stopOnWeight() {
     if(runningCfg.stopOnWeightState && runningCfg.shotStopOnCustomWeight < 1.f) {
       if (shotWeight > (shotTarget - 2.0f)) {
         if (scalesIsPresent && preinfusionFinished) brewStopWeight = shotWeight + currentState.weightFlow * 10.f;
-        else brewStopWeight = shotWeight + (currentState.pumpFlow*2.f * 10.f);
+        else brewStopWeight = shotWeight + (smoothedPumpFlow * 2.f * 10.f);
         return true;
       } else return false;
     } else if(runningCfg.stopOnWeightState && runningCfg.shotStopOnCustomWeight > 1.f) {
       if (shotWeight > runningCfg.shotStopOnCustomWeight-2.0f) {
         if (scalesIsPresent && preinfusionFinished) brewStopWeight = shotWeight + currentState.weightFlow * 10.f;
-        else brewStopWeight = shotWeight + (currentState.pumpFlow*2.f * 10.f);
+        else brewStopWeight = shotWeight + (smoothedPumpFlow * 2.f * 10.f);
         return true;
       } else return false;
     }
