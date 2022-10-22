49a4cc61fce54b3c6c845a1b5edd7fefcbf940de
mr.toor <42692077+Zer0-bit@users.noreply.github.com>
2022-10-22 16:09:54 +0100

    Delete secrets.h

--- webserver/src/secrets.h
+++
@@ -1,9 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define WIFI_PASSWORD a86831e510
-#define WIFI_SSID MI5
-#define BASIC_AUTH_USERNAME gaggiuino
-#define BASIC_AUTH_PASSWORD oniuiggag
-
-#endif
