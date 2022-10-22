f67684def21b64e46d803d371d4f68417b3131fd
Zer0-bit <toor@root.com>
2022-10-22 17:05:59 +0100

    fixing webserver build

--- webserver/src/server.cpp
+++ webserver/src/server.cpp
@@ -1,7 +1,7 @@
 #include "server.h"
 
-const char* ssid = "SSID";
-const char* password =  "WIFI_PASSWORD";
+const char* ssid = WIFI_SSID;
+const char* password =  WIFI_PASSWORD;
 
 #define FORMAT_LITTLEFS_IF_FAILED   true
 
@@ -58,7 +58,7 @@ void setup(){
     loadFSJavaScript();
     loadFSStatic();
 
-    firmwareUpdate();
+    // firmwareUpdate();
     // Handle Web Server Events
     events.onConnect([](AsyncEventSourceClient *client){
     if(client->lastId()){
@@ -75,40 +75,6 @@ void setup(){
     });
 
        // Simple Firmware Update Form
-    server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request)
-        {
-            request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
-        }
-    );
-    server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request) 
-        {
-            shouldReboot = !Update.hasError();
-            AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
-            response->addHeader("Connection", "close");
-            request->send(response);
-        },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
-        {
-            if(!index){
-                Serial.printf("Update Start: %s\n", filename.c_str());
-                Update.runAsync(true);
-                if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
-                Update.printError(Serial);
-                }
-            }
-            if(!Update.hasError()){
-                if(Update.write(data, len) != len){
-                Update.printError(Serial);
-                }
-            }
-            if(final){
-                if(Update.end(true)){
-                Serial.printf("Update Success: %uB\n", index+len);
-                } else {
-                Update.printError(Serial);
-                }
-            }
-        }
-    );
     
     server.onNotFound(onRequest);
     server.onFileUpload(onUpload);
@@ -224,4 +190,41 @@ void espReboot() {
     static char temp[128];
     sprintf(temp, "Seconds since boot: %u", millis()/1000);
     events.send(temp, "time"); //send event "time"
+}
+
+void firmwareUpdate() {
+    server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request)
+        {
+            request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
+        }
+    );
+    // server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request) 
+    //     {
+    //         shouldReboot = !Update.hasError();
+    //         AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
+    //         response->addHeader("Connection", "close");
+    //         request->send(response);
+    //     },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+    //     {
+    //         if(!index){
+    //             Serial.printf("Update Start: %s\n", filename.c_str());
+    //             Update.runAsync(true);
+    //             if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
+    //             Update.printError(Serial);
+    //             }
+    //         }
+    //         if(!Update.hasError()){
+    //             if(Update.write(data, len) != len){
+    //             Update.printError(Serial);
+    //             }
+    //         }
+    //         if(final){
+    //             if(Update.end(true)){
+    //             Serial.printf("Update Success: %uB\n", index+len);
+    //             } else {
+    //             Update.printError(Serial);
+    //             }
+    //         }
+    //     }
+    // );
 }
