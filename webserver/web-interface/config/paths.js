3c5ebceda1e5cf50a1e77bab880234becce4dbea
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2022-10-28 00:59:53 +0800

    Remove data files and make npm run build save dist to ../data dir

--- webserver/web-interface/config/paths.js
+++ webserver/web-interface/config/paths.js
@@ -21,7 +21,7 @@ const publicUrlOrPath = getPublicUrlOrPath(
   process.env.PUBLIC_URL
 );
 
-const buildPath = process.env.BUILD_PATH || 'build';
+const buildPath = process.env.BUILD_PATH || '../data';
 
 const moduleFileExtensions = [
   'web.mjs',
