2f9565365814ac1a3b5fdb9a0825751b79d07d8f
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2022-11-08 01:07:52 +0800

    Improve tab bar to reflect active tab on refresh correctly

--- webserver/web-interface/src/components/appbar/TabsBar.jsx
+++ webserver/web-interface/src/components/appbar/TabsBar.jsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import CoffeeIcon from '@mui/icons-material/Coffee';
@@ -9,27 +9,42 @@ import Box from '@mui/material/Box';
 import {
   useTheme, Stack, AppBar, Toolbar,
 } from '@mui/material';
+import PropTypes from 'prop-types';
 import Logo from '../icons/Logo';
 import ThemeModeToggle from '../theme/ThemeModeToggle';
 
-function TabsBar() {
+function LinkTab(props) {
+  const { value: path } = props;
   const navigate = useNavigate();
+  const location = useLocation();
   const theme = useTheme();
-  const [value, setValue] = React.useState(0);
 
   const textColor = theme.palette.text.secondary;
   const activeColor = theme.palette.mode === 'light' ? theme.palette.primary.contrastText : theme.palette.primary.main;
+  const id = path.replace('/', '');
 
-  function handleProps(index) {
-    return {
-      id: `appbar-tab-${index}`,
-      'aria-controls': `appbar-tabpanel-${index}`,
-      sx: { color: value === index ? activeColor : textColor, opacity: 1 },
-    };
-  }
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  return (
+    <Tab
+      {...props}
+      iconPosition="start"
+      id={`appbar-tab-${id}`}
+      sx={{ color: location.pathname === path ? activeColor : textColor, opacity: 1 }}
+      onClick={() => navigate(path)}
+    >
+    </Tab>
+  );
+}
+
+LinkTab.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+function TabsBar() {
+  const theme = useTheme();
+  const location = useLocation();
+  const [activeTab, setActiveTab] = useState(location.pathname || '/');
+
+  const activeColor = theme.palette.mode === 'light' ? theme.palette.primary.contrastText : theme.palette.primary.main;
 
   return (
     <AppBar sx={{ position: 'static' }} elevation={1}>
@@ -42,8 +57,8 @@ function TabsBar() {
             <Logo size={40} />
           </Box>
           <Tabs
-            value={value}
-            onChange={handleChange}
+            value={activeTab}
+            onChange={(event, value) => setActiveTab(value)}
             aria-label="Navigation tabs"
             textColor="inherit"
             TabIndicatorProps={{
@@ -52,9 +67,9 @@ function TabsBar() {
               },
             }}
           >
-            <Tab icon={<CoffeeIcon />} iconPosition="start" label="Home" {...handleProps(0)} onClick={() => navigate('/')} />
-            <Tab icon={<TuneIcon />} iconPosition="start" label="Profiles" {...handleProps(1)} onClick={() => navigate('/profiles')} />
-            <Tab icon={<SettingsIcon />} iconPosition="start" label="Settings" {...handleProps(2)} onClick={() => navigate('/settings')} />
+            <LinkTab icon={<CoffeeIcon />} label="Home" value="/" />
+            <LinkTab icon={<TuneIcon />} label="Profiles" value="/profiles" />
+            <LinkTab icon={<SettingsIcon />} label="Settings" value="/settings" />
           </Tabs>
           <ThemeModeToggle />
         </Stack>
