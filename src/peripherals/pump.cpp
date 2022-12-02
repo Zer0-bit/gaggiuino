8b7cafb5bcb9d8c4e19a905faca6eda5fc9feb61
Zer0-bit <toor@root.com>
2022-12-02 10:55:02 +0000

    more range correction for the predictive scales

--- src/peripherals/pump.cpp
+++ src/peripherals/pump.cpp
@@ -91,9 +91,9 @@ long getAndResetClickCounter(void) {
 float getFlowPerClick(float pressure) {
     float fpc;
     if (pressure >= 0.f && pressure <= 6.f) {
-      fpc = mapRange(pressure, 0.f, 6.f, 640.f, 340.f, 1) / 50 / maxPumpClicksPerSecond;
+      fpc = mapRange(pressure, 0.f, 6.f, 640.f, 350.f, 1) / 50 / maxPumpClicksPerSecond;
     } else if (pressure > 6.f && pressure <= 8.f) {
-      fpc = mapRange(pressure, 6.f, 8.f, 341.f, 300.f, 1) / 50 / maxPumpClicksPerSecond;
+      fpc = mapRange(pressure, 6.f, 8.f, 351.f, 300.f, 1) / 50 / maxPumpClicksPerSecond;
     } else if (pressure > 8.f && pressure <= 12.f) {
       fpc = mapRange(pressure, 8.f, 12.f, 301.f, 105.f, 1) / 50 / maxPumpClicksPerSecond;
     } else {
