9b46433520a84bf72f79019284e13d437061a2bf
Zer0-bit <toor@root.com>
2023-01-04 00:28:41 +0000

    tests fix

---
+++ test/IWatchdog.h
@@ -0,0 +1,14 @@
+class IWatchdogClass {
+
+  public:
+    void begin(uint32_t timeout, uint32_t window = IWDG_TIMEOUT_MAX);
+    void set(uint32_t timeout, uint32_t window = IWDG_TIMEOUT_MAX);
+    void get(uint32_t *timeout, uint32_t *window = NULL);
+    void reload(void);
+    bool isEnabled(void)
+    {
+      return _enabled;
+    };
+    bool isReset(bool clear = false);
+    void clearReset(void);
+};
