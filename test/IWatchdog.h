3f3fa84121d029a4a470dfd47171b1d1a1d6fe84
Zer0-bit <toor@root.com>
2023-01-04 06:42:26 +0000

    having one default

--- test/IWatchdog.h
+++ test/IWatchdog.h
@@ -1,8 +1,8 @@
 class IWatchdogClass {
 
   public:
-    void begin(uint32_t timeout, uint32_t window);
-    void set(uint32_t timeout, uint32_t window);
+    void begin(uint32_t timeout, uint32_t window = 3000);
+    void set(uint32_t timeout, uint32_t window = 3000);
     void get(uint32_t *timeout, uint32_t *window = NULL);
     void reload(void);
     bool isEnabled(void)
