a722bd302388d092ef1d60424e3af4cb0d5d50d7
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2023-01-05 22:50:19 +0800

    Some more data logging in test sketches

--- uart-esp/main.ino
+++ uart-esp/main.ino
@@ -40,6 +40,7 @@ void loop(void) {
   comms.readData();
   if (millis() - profileTimer > 8000) {
     profile.count = 5;
+    profile.globalStopConditions = GlobalStopConditions { .time=1923, .weight=37.f, .waterPumped=60.f};
     profile.phases[0] = Phase{ PHASE_TYPE_FLOW, Transition(4.5f), 2.f, PhaseStopConditions{ .pressureBelow=2.f } };
     profile.phases[1] = Phase{ PHASE_TYPE_FLOW, Transition(0.f), 2.f, PhaseStopConditions{ .time=3000 } };
     profile.phases[2] = Phase{ PHASE_TYPE_PRESSURE, Transition(9.f), -1, PhaseStopConditions{ .time=30000 } };
