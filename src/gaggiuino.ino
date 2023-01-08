efd02bc21d22835c1a38aa04b80832732d7e6057
Zer0-bit <toor@root.com>
2023-01-08 10:29:37 +0000

    changes to isPressureRisingFast comparison val

--- src/gaggiuino.ino
+++ src/gaggiuino.ino
@@ -130,7 +130,7 @@ static void sensorsReadPressure(void) {
     previousSmoothedPressure = currentState.smoothedPressure;
     currentState.pressure = getPressure();
     currentState.isPressureRising = isPressureRaising();
-    currentState.isPressureRisingFast = currentState.smoothedPressure >= previousSmoothedPressure + 0.55f;
+    currentState.isPressureRisingFast = currentState.smoothedPressure >= previousSmoothedPressure + 2.55f;
     currentState.isPressureFalling = isPressureFalling();
     currentState.isPressureFallingFast = isPressureFallingFast();
     currentState.smoothedPressure = smoothPressure.updateEstimate(currentState.pressure);
