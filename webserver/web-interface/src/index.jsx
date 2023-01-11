32eefd438a51ba70140b3ba7e38ee3031a4094ed
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2023-01-11 09:14:20 +0800

    Revert "Add service worker to enable caching of files in production mode"
    This reverts commit 1f093a5938d9f35d72aae186c3ec33ebe3827bc9.

--- webserver/web-interface/src/index.jsx
+++ webserver/web-interface/src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,5 +8,3 @@ root.render(
     <App />
   </React.StrictMode>,
 );
-
-serviceWorkerRegistration.register();
