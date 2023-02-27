2ae800e19ed91035e407bc3ca9e0fa04ef2781ea
banoz <banoz@users.noreply.github.com>
2023-02-27 12:27:14 -0600

    ffs

--- test-lib/mocks/PSM.cpp
+++ test-lib/mocks/PSM.cpp
@@ -10,9 +10,9 @@ void PSM::resetCounter() {
 }
 void PSM::stopAfter(long counter) {
 }
-unsigned int cps() {
+unsigned int PSM::cps() {
   return 50u;
 }
-unsigned long getLastMillis() {
+unsigned long PSM::getLastMillis() {
   return 1ul;
 }
