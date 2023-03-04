81d8ed433cb3d24a2e219369a23caa8dcf6b09b5
Zer0-bit <toor@root.com>
2023-03-04 22:45:43 +0000

    returning to old formula style

--- src/peripherals/pump.cpp
+++ src/peripherals/pump.cpp
@@ -89,11 +89,12 @@ int getCPS(void) {
 // float fpc = (flowPerClickAtZeroBar - pressureInefficiencyConstant0) + (pressureInefficiencyConstant1 + (pressureInefficiencyConstant2 + (pressureInefficiencyConstant3 + (pressureInefficiencyConstant4 + (pressureInefficiencyConstant5 + pressureInefficiencyConstant6 * pressure) * pressure) * pressure) * pressure) * pressure) * pressure;
 // Polinomyal func that should in theory calc fpc faster than the above.
 float getPumpFlowPerClick(float pressure) {
-  float fpc = flowPerClickAtZeroBar - pressureInefficiencyCoefficient[0];
-  for (int i = 6; i >= 1; i--) {
-      fpc += pressureInefficiencyCoefficient[i] * pressure;
-  }
   // float fpc = (flowPerClickAtZeroBar - pressureInefficiencyCoefficient[0]) + (pressureInefficiencyCoefficient[1] + (pressureInefficiencyCoefficient[2] + (pressureInefficiencyCoefficient[3] + (pressureInefficiencyCoefficient[4] + (pressureInefficiencyCoefficient[5] + pressureInefficiencyCoefficient[6] * pressure) * pressure) * pressure) * pressure) * pressure) * pressure;
+  float fpc = pressureInefficiencyCoefficient[6] * pressure;
+  fpc = ((fpc + pressureInefficiencyCoefficient[5]) * pressure + pressureInefficiencyCoefficient[4]) * pressure;
+  fpc = ((fpc + pressureInefficiencyCoefficient[3]) * pressure + pressureInefficiencyCoefficient[2]) * pressure;
+  fpc = (fpc + pressureInefficiencyCoefficient[1]) * pressure + flowPerClickAtZeroBar - pressureInefficiencyCoefficient[0];
+
   return 50.f * fmaxf(fpc, 0.f) / (float)maxPumpClicksPerSecond;
 }
 
