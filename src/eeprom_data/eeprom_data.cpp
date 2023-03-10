cf5d2b0747c3c09c08c97128979301680764bafc
Zer0-bit <toor@root.com>
2023-03-10 11:59:18 +0000

    up the default PZ val to a middle ground best

--- src/eeprom_data/eeprom_data.cpp
+++ src/eeprom_data/eeprom_data.cpp
@@ -48,7 +48,7 @@ namespace {
     defaultData.basketPrefill = false;
     defaultData.scalesF1 = 3920;
     defaultData.scalesF2 = 4210;
-    defaultData.pumpFlowAtZero = 0.285f;
+    defaultData.pumpFlowAtZero = 0.295f;
     defaultData.stopOnWeightState = false;
     defaultData.shotDose = 18.0f;
     defaultData.shotStopOnCustomWeight = 0.f;
