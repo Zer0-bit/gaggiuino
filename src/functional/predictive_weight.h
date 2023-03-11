ea3d2fd6bfc237ebb0d2c11fb32259df6253ca14
Zer0-bit <toor@root.com>
2023-03-11 16:13:24 +0000

    pp flow discard

--- src/functional/predictive_weight.h
+++ src/functional/predictive_weight.h
@@ -49,6 +49,8 @@ public:
     if (phaseTypePressure) {
       // If the pressure or flow are raising too fast dismiss the spike from the output.
       if (state.isPressureRisingFast || state.isPumpFlowRisingFast) return;
+      // If flow is too big for given pressure or the delta is changing too quickly we're not there yet
+      if (resistanceDelta > 500.f || puckResistance < 1100.f) return;
     }
 
     // If flow is too big for given pressure or the delta is changing too quickly we're not there yet
