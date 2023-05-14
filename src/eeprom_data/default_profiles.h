fd166d79c79ccc1bd52d02cc1d407ccd39b9b347
Zer0-bit <toor@root.com>
2023-05-14 13:38:30 +0100

    filter 2.1 defaults change

--- src/eeprom_data/default_profiles.h
+++ src/eeprom_data/default_profiles.h
@@ -52,7 +52,7 @@ namespace {
     {"IUIUIU Classic", true, true, 10, 2.f, 3.f, 20, 4.f, 3.f, 600, true, 4.f, true, 7, 30, 0.f, 0.f, 2.f, 0.f, 4.f, 5, 2, true, true, 9.f, 6.f, 1, 3.f, 15, 0, 3.f, 2.5f, 1.f, 1, 7.5f, 15, 0, 6.f},  // profile 0
     {"Londinium", true, true, 10, 2.f, 3.f, 20, 4.f, 9.f, 650, true, 0.f, true, 7, 10, 0.f, 0.f, 0.7f, 0.f, 0.f, 5, 2, true, false, 9.f, 3.f, 1, 3.f, 20, 0, 3.f, 2.5f, 1.f, 1, 7.5f, 15, 0, 6.f},  // profile 1
     {"D-Flow", true, true, 10, 2.f, 3.f, 20, 3.f, 7.f, 600, true, 0.f, true, 7, 6, 3.f, 0.8f, 0.f, 0.f, 0.f, 5, 2, true, true, 9.f, 6.f, 1, 3.f, 15, 0, 3.f, 2.f, 2.f, 1, 7.f, 15, 0, 7.f},  // profile 2
-    {"Filter 2.1", true, true, 10, 2.f, 3.f, 5, 0.f, 4.f, 600, true, 0.f, true, 7, 90, 0.f, 0.2f, 0.f, 0.f, 45.f, 0, 0, true, true, 9.f, 6.f, 1, 3.f, 15, 0, 3.f, 0.2f, 3.f, 0, 0.f, 8, 0, 0.f},  // profile 3
+    {"Filter 2.1", true, true, 10, 2.f, 3.f, 15, 1.f, 4.f, 600, true, 0.f, true, 7, 90, 0.f, 0.2f, 0.f, 0.f, 45.f, 0, 0, true, true, 9.f, 6.f, 1, 3.f, 15, 0, 3.f, 0.2f, 3.f, 0, 0.f, 8, 0, 0.f},  // profile 3
     {"Blooming espresso", true, true, 10, 2.f, 3.f, 20, 7.f, 4.f, 650, true, 0.f, true, 7, 30, 0.f, 0.f, 0.6f, 0.f, 5.f, 5, 2, true, true, 9.f, 6.f, 1, 3.f, 15, 0, 2.f, 2.f, 2.f, 1, 9.f, 15, 4, 9.f}  // profile 4
   };
 }
