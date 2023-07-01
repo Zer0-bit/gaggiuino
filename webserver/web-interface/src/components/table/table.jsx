9be9669ec535c9f5eb21d695adad5daee5a6b3ec
Zer0-bit <toor@root.com>
2023-07-01 14:00:31 +0100

    more

--- webserver/web-interface/src/components/table/table.jsx
+++ webserver/web-interface/src/components/table/table.jsx
@@ -10,7 +10,7 @@ export default function DataTable() {
         columns={tableColumns}
         pageSize={20}
         rowsPerPageOptions={[5]}
-        checkboxSelection
+        // checkboxSelection
       />
     </div>
   );
