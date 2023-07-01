6dbc85d085c52f5ad6aa9ef586cf223eac9bfb98
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2023-07-01 23:01:43 +0800

    Remove eeprom stuff from STM

--- src/eeprom_data/eeprom_base.h
+++
@@ -1,8 +0,0 @@
-#ifndef BASE_EEPROM_DATA_H
-#define BASE_EEPROM_DATA_H
-
-// BaseValue class extended by all value structs.
-// This is used to support migrations from different versions
-struct BaseValues_t {};
-
-#endif
