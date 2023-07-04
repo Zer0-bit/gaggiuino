8ce6490c49a03e134fcd427c956847621750b0d0
Zer0-bit <toor@root.com>
2023-07-04 15:51:56 +0100

    del unused button import

--- webserver/web-interface/src/components/alert/alert.tsx
+++ webserver/web-interface/src/components/alert/alert.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
