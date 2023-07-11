4abd56e45b6079e955868ab775b8f115c82066df
Kostas Stamatoukos <kostas.stamatoukos@transferwise.com>
2023-07-11 09:46:47 +0800

    Add ability to change dev ip with untracked file

--- webserver/web-interface/vite.config.ts
+++ webserver/web-interface/vite.config.ts
@@ -1,21 +1,39 @@
+/* eslint-disable no-console */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 
-const targetIp = '192.168.0.21';
+// This function gets the IP that the Development server will point to from `local.config.ts`.
+// To change your local IP create a file named `local.config.ts` in the same directory as vite.config.ts
+// And contents:
+// -------------------------------------------------
+// const localConfig = { targetIp: '192.168.0.21' };
+// export default localConfig;
+// -------------------------------------------------
+async function getDevelopmentIp() {
+  const defaultTargetIp = '192.168.4.1';
+  try {
+    const localConfig = await import('./local.config');
+    console.info(`Development server proxying to ${localConfig.default.targetIp}`);
+    return localConfig.default.targetIp;
+  } catch (e) {
+    console.info(`Didn not find local_config.ts file. IP will default to ${defaultTargetIp}`);
+    return defaultTargetIp;
+  }
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(async () => ({
   server: {
     proxy: {
       '/api': {
-        target: `http://${targetIp}`,
+        target: `http://${await getDevelopmentIp()}`,
         changeOrigin: true,
         secure: false,
       },
       '/ws': {
-        target: `ws://${targetIp}`,
+        target: `ws://${await getDevelopmentIp()}`,
         ws: true,
         changeOrigin: true,
         secure: false,
@@ -29,4 +47,4 @@ export default defineConfig({
     outDir: '../data',
     emptyOutDir: true,
   },
-});
+}));
