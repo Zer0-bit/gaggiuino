5a6f2311a6d871ab545fd6275e30fe18df026777
Zer0-bit <toor@root.com>
2023-07-16 15:33:44 +0100

    kalman

--- src/gaggiuino.ino
+++ src/gaggiuino.ino
@@ -7,7 +7,7 @@
 #include "gaggiuino.h"
 
 SimpleKalmanFilter smoothPressure(0.6f, 0.6f, 0.06f);
-SimpleKalmanFilter smoothPumpFlow(0.6f, 0.6f, 0.06f);
+SimpleKalmanFilter smoothPumpFlow(0.6f, 0.6f, 0.006f);
 SimpleKalmanFilter smoothScalesFlow(0.5f, 0.5f, 0.01f);
 SimpleKalmanFilter smoothConsideredFlow(0.1f, 0.1f, 0.1f);
 
