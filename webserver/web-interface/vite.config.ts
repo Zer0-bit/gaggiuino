0519795beb4839bae945baf10d1d710b489d99a0
Zer0-bit <toor@root.com>
2023-07-20 16:22:09 +0100

    inb4 pwa

--- webserver/web-interface/vite.config.ts
+++ webserver/web-interface/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
 import viteCompression from 'vite-plugin-compression';
+import { VitePWA } from 'vite-plugin-pwa'
 // This function gets the IP that the Development server will point to from `local.config.ts`.
 // To change your local IP create a file named `local.config.ts` in the same directory as vite.config.ts
 // And contents:
@@ -46,7 +47,14 @@ export default defineConfig(async () => ({
     react(),
     viteTsconfigPaths(),
     svgrPlugin(),
-    viteCompression()
+    viteCompression(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true
+      }
+    })
   ],
   build: {
     outDir: '../data',
